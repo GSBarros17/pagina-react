00844dcc2c9a9a712bd7a52290cc05a75fe781cf
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-06 11:02:06 -0300

    organização da pasta e teste de componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import '../App.css';
+import './App.css';
+import Teste from './components/Teste.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Olá este é meu novo projeto</h1>
+        <Teste 
+          name="Guilherme"
+        />
       </header>
     </div>
   );
---
+++ src/components/Teste.js
@@ -0,0 +1,7 @@
+export default function Teste({name}    ){
+    return(
+        <h1>
+            Olá {name}, como vai você?
+        </h1>
+    )
+}
