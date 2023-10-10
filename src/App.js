649dcf820d415a99204be854bad2e2efcd9b3510
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-10 16:47:42 -0300

    criando as pgs do projeto e sua rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { BrowserRouter as Router, Routes,BrowserRouter, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home"
+import Company from "./components/pages/Company"
+import Contact from "./components/pages/Contact"
+import NewProject from "./components/pages/NewProject"
 
 function App() {
   return (
-    <p>Novo projeto!</p>
+    <BrowserRouter>
+     <ul>
+        <Link to="/">Home</Link>
+        <Link to="/Company">Empresa</Link>
+        <Link to="/Contact">Contato</Link>
+        <Link to="/NewProject">Novo Projeto</Link>
+      </ul>
+
+          <Routes>
+            <Route path= "/" element={<Home/>}/> 
+            <Route path= "/Company" element={<Company/>}/> 
+            <Route path= "/Contact" element={<Contact/>}/> 
+            <Route path= "/NewProject" element={<NewProject/>}/>    
+          </Routes>
+
+      <p>Footer</p>
+    </BrowserRouter>
   );
 }
 
