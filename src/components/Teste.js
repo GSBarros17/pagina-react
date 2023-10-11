2bf61e7124d6984a8143b2b92d5a53d6faef0a78
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-11 16:13:35 -0300

    criando e estilizando o footer

--- src/components/Teste.js
+++
@@ -1,7 +0,0 @@
-export default function Teste({name}    ){
-    return(
-        <h1>
-            Olá {name}, como vai você?
-        </h1>
-    )
-}
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,21 @@
-import styler from "./Footer.module.css"
+import styles from "./Footer.module.css"
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export default function Footer(){
-    return <p>Footer</p>
+    return (
+        <footer className={styles.footer}>
+            <ul className={styles.socialMedia}>
+                <li>
+                    <FaFacebook/>
+                </li>
+                <li>
+                    <FaInstagram/>
+                </li>
+                <li>
+                    <FaLinkedin/>
+                </li>
+            </ul>
+            <p className={styles.text}><span>Finance </span>©️ 2023</p>
+        </footer>
+    )
 }
