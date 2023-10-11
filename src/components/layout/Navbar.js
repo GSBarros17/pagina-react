0dc4737acd0b87d03d8c001aac2f29b94fa1f03c
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-11 09:56:09 -0300

    alterando a logo do projeto

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import styles from "./Navbar.module.css"
 import Container from "./Container"
-import logo from "../img/Logo.svg"
+import logo from "../img/logo.svg"
 
 export default function Navbar(){
     return (
@@ -10,7 +10,7 @@ export default function Navbar(){
                 <Link to="/">
                     <img className={styles.logoNavbar} src={logo} alt="logo Finance Project" />
                 </Link>
-                <ul>
+                <ul className={styles.list}>
                     <li>
                         <Link to="/">Home</Link>
                     </li>
