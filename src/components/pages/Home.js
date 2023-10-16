11711415979873c466ef8d20a95cf628fff29956
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-16 08:43:17 -0300

    add elementos a página home e seus estilos

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,14 @@
+import  styles  from "./Home.module.css"
+import { Link } from "react-router-dom"
+import ImgBanner from "../img/img1.svg"
+
 export default function Home(){
     return (
-        <div>
-            <h1>Home</h1>
-        </div> 
+        <section className={styles.containerHome}>
+            <h1>Bem vindo ao <span>FINANCE</span>.</h1>
+            <p>Gerencie seus projetos com apenas um clique!</p>
+            <a href="/">criar projeto</a>
+            <img className={styles.imgHome} src={ImgBanner} alt="banner principal finance" />
+        </section>
     )
 }
