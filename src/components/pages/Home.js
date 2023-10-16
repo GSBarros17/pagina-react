c4936ae400a4159718a52fb0c4aa447e2a1ae292
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-16 09:00:55 -0300

    criando o linkbutton e seu estilo

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,13 @@
-import  styles  from "./Home.module.css"
-import { Link } from "react-router-dom"
+import styles from "./Home.module.css"
 import ImgBanner from "../img/img1.svg"
+import LinkButton from "../layout/LinkButton"
 
 export default function Home(){
     return (
         <section className={styles.containerHome}>
             <h1>Bem vindo ao <span>FINANCE</span>.</h1>
             <p>Gerencie seus projetos com apenas um clique!</p>
-            <a href="/">criar projeto</a>
+            <LinkButton to="/NewProjects" text="Criar projeto" />
             <img className={styles.imgHome} src={ImgBanner} alt="banner principal finance" />
         </section>
     )
