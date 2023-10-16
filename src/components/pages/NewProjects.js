930f87042526d264732b8d741cbd18957536c223
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-16 14:48:16 -0300

    desenvolvendo o projectform

--- src/components/pages/NewProjects.js
+++ src/components/pages/NewProjects.js
@@ -1,11 +1,12 @@
 import styles from "./NewProjects.module.css"
+import ProjectForm from "../project/ProjectForm"
 
 export default function NewProjects(){
     return (
         <div className={styles.newprojectContainer}>
             <h1>Criar projeto</h1>
             <p>Crie seu projeto e gerencie-o</p>
-
+            <ProjectForm />
         </div>
     )
 }
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,7 +1,22 @@
 export default function ProjectForm(){
     return(
         <div>
-            <h1>este é o criador de projetos</h1>
+           <form>
+            <div>
+                <input type="text" placeholder="Insira o nome do projeto" />
+            </div>
+            <div>
+                <input type="number" placeholder="Insira o valor do projeto" />
+            </div>
+            <div>
+                <select name="category-id">
+                    <option disabled>Selecione a categoria</option>
+                </select>
+            </div>
+            <div>
+                <input type="submit" value="Adicionar projeto" />
+            </div>
+           </form>
         </div>
     )
 }
