316d16e7561ca96aa5823e5b94b5e89f0924c443
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-16 09:10:59 -0300

    alterando o css da página home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ import LinkButton from "../layout/LinkButton"
 export default function Home(){
     return (
         <section className={styles.containerHome}>
-            <h1>Bem vindo ao <span>FINANCE</span>.</h1>
+            <h1>Bem vindo ao <span>FINANCE</span></h1>
             <p>Gerencie seus projetos com apenas um clique!</p>
             <LinkButton to="/NewProjects" text="Criar projeto" />
             <img className={styles.imgHome} src={ImgBanner} alt="banner principal finance" />
