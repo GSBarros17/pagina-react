7434961c507ead817d5e102f94b34e77e9125fcb
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-17 14:32:56 -0300

    criando o componente Message

---
+++ src/components/pages/Message.js
@@ -0,0 +1,7 @@
+import styles from "./Message.module.css"
+
+export default function Message(){
+    return (
+        <p>essa é uma mensagem</p>
+    )
+}
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,9 @@
 import styles from "./Projects.module.css"
 
 export default function Projects(){
-    return <h1>Projects</h1>
+    return (
+        <div>
+            <h1>Meus Projetos</h1>
+        </div>
+    )
 }
