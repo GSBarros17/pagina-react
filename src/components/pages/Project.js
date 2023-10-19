6a47fbbe24b827a02bf1a89eba9ef3a13f755985
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-19 15:16:34 -0300

    add parametro para deixar a pg de edição dinamica

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,7 +1,32 @@
 import styles from "./Project.module.css"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
 
 export default function Project(){
+    
+    const {id} = useParams()
+    const [project, setProject] = useState()
+
+    useEffect(()=> {
+        fetch(`http://localhost:5000/projects/${id}`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setProject(data)
+        })
+        .catch((err) => console.log(err))
+    }, [id])
+    
+    
     return(
-        <p>Porjeto</p>
+        <div>
+        {project &&(
+             <p>{project.Name_project}</p>
+         )}
+    </div>
     )
 }
