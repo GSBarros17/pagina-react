108bd1d6356193d15b8ea62035a8c23aed725ca7
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-20 16:29:52 -0300

    criando estile e dev a pag Company

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -1,3 +1,14 @@
+import {Link} from "react-router-dom"
+import styles from "./Company.module.css"
+import LinkButton from "../layout/LinkButton"
+
 export default function Company(){
-    return <h1>Company</h1>
+    return (
+        <div>
+            <div>
+                <h1>Historia da Finance</h1>
+            </div>
+            <LinkButton to="/NewProjects" text="Criar projeto"/>
+        </div>
+    )
 }
