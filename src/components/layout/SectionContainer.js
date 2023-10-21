324a164174ad07a2253fcc864c069f6d65c57093
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-21 15:41:48 -0300

    add span com um icone de check na sectioncontainer

--- src/components/layout/SectionContainer.js
+++ src/components/layout/SectionContainer.js
@@ -1,11 +1,12 @@
 import styles from "./SectionContainer.module.css"
+import { BsCheckLg } from "react-icons/bs"
 
 export default function SectionContainer({img , altText, text1, text2}){
     return (
         <div className={styles.containerSection}>
             <div>
-                <p>{text1}</p>
-                <p>{text2}</p>
+                <p><span><BsCheckLg/></span>{text1}</p>
+                <p><span><BsCheckLg/></span>{text2}</p>
             </div>
             <div>
                 <img src={img} alt={altText} />
