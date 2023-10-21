ddae0a49523726a31159747de92d8c38b8e9b931
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-21 15:27:21 -0300

    alterando o estilo da pg company

--- src/components/layout/SectionContainer.js
+++ src/components/layout/SectionContainer.js
@@ -1,10 +1,11 @@
 import styles from "./SectionContainer.module.css"
 
-export default function SectionContainer({img , altText, text}){
+export default function SectionContainer({img , altText, text1, text2}){
     return (
         <div className={styles.containerSection}>
             <div>
-                <p>{text}</p>
+                <p>{text1}</p>
+                <p>{text2}</p>
             </div>
             <div>
                 <img src={img} alt={altText} />
--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -10,15 +10,10 @@ export default function Company(){
                 <h1>Sobre a <span>FINANCE</span></h1>
             </div>
             <LinkButton to="/NewProjects" text="Criar projeto"/>
-            <SectionContainer text="Esse é um texte muito legal de se fazer, você não saber a 
-            minha felicidade de estar aqui, contanto um pouco da minha história, trabalhei 7 
-            anos em um banco, mais em um belo dia me dei conta de que aquilo não era para mim
-            então, resolvi mudar todo o rumo da minha vida e comecei a estudar programação
-            e sai do banco para tentar seguir este novo desafio, e estou aqui crianco este
-            projeto, a Finance e um projeto criado em paralelo a um curso de React do prof.
-            Matheus Battisti. A Finance é um site de gestão de processos da área de Ti
-            em que o usuário cadastra o seu projeto e acompanha o quando já utilizou do
-            orçamento em serviços e após o termino o projeto pode ser encerrado." 
+            <SectionContainer text1="Finance nasceu de um projeto criado em paralelo a um curso de React do prof.
+            Matheus Battisti." text2="A Finance é um site de gestão de processos da área de TI
+            em que o usuário cadastra o seu projeto e acompanha o quanto já utilizou do
+            orçamento em serviços." 
             img={img2} altText={"banner2 Finance"} />
         </div>   
     )
