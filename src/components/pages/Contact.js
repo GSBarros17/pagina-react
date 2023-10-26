b7cff19b028f1487154f3f27cead23a8c7f11001
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-26 11:01:40 -0300

    criando a area de socialMidia na pg contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,7 @@
+import { BsPaperclip , BsGlobe2 , BsGithub , BsWhatsapp , BsLinkedin } from "react-icons/bs"
 import styles from "./Contact.module.css"
 import Devimg from "../img/devimg.jpg"
-
+import Docdev from "../../docs/curriculoGuilherme.docx"
 export default function Contact(){
     return (
         <div className={styles.containerContact}>
@@ -11,7 +12,14 @@ export default function Contact(){
                     <h2>Olá, me chamo <span>Guilherme Barros.</span></h2>
                     <p>Sou o responsável por desenvolver esta página!</p>
                     <h3>Caso você tenha interesse no meu trabalho entre em contato comigo pelas minhas redes sociais:</h3>
-                </div> 
+                    <div className={styles.socialMidia}>
+                        <a aria-label="currículo do desenvolvedor" href={Docdev}><BsPaperclip/></a>
+                        <a aria-label="link do site do desenvolvedor"href="https://www.devguilhermebarros.com.br"><BsGlobe2/></a>
+                        <a aria-label="link para whatsapp"href="https://wa.me/5561999767446"><BsWhatsapp/></a>
+                        <a aria-label="link do github do desenvolvedor" href="https://github.com/GSBarros17"><BsGithub/></a>
+                        <a aria-label="link do linkedin desenvolvedor" href="www.linkedin.com/in/guilherme-barros-b05632294"><BsLinkedin/></a>
+                    </div>
+                </div>
             </div>
         </div>
     )
