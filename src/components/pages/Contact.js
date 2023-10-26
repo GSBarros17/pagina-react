4abfbd21606bb45c73b51d0aaffc49fd53627061
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-26 11:10:49 -0300

    alterando o link da tag linkedin

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -11,13 +11,14 @@ export default function Contact(){
                 <div className={styles.sectionTexts}>
                     <h2>Olá, me chamo <span>Guilherme Barros.</span></h2>
                     <p>Sou o responsável por desenvolver esta página!</p>
-                    <h3>Caso você tenha interesse no meu trabalho entre em contato comigo pelas minhas redes sociais:</h3>
+                    <h3>Caso você tenha interesse no meu trabalho,
+                    entre em contato comigo pelas minhas redes sociais:</h3>
                     <div className={styles.socialMidia}>
                         <a aria-label="currículo do desenvolvedor" href={Docdev}><BsPaperclip/></a>
                         <a aria-label="link do site do desenvolvedor"href="https://www.devguilhermebarros.com.br"><BsGlobe2/></a>
                         <a aria-label="link para whatsapp"href="https://wa.me/5561999767446"><BsWhatsapp/></a>
                         <a aria-label="link do github do desenvolvedor" href="https://github.com/GSBarros17"><BsGithub/></a>
-                        <a aria-label="link do linkedin desenvolvedor" href="www.linkedin.com/in/guilherme-barros-b05632294"><BsLinkedin/></a>
+                        <a aria-label="link do linkedin desenvolvedor" href="https://www.linkedin.com/in/guilherme-barros-b05632294"><BsLinkedin/></a>
                     </div>
                 </div>
             </div>
