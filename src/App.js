ccdd7752dabfb452c99a24ae4d5b498fe5a22d17
Guilherme Barros <gsbguilherme23@gmail.com>
2023-12-19 15:21:55 -0300

    Criando pastas e organizando a pasta page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes,BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter , Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/pages/Home"
 import Company from "./components/pages/Company"
 import Contact from "./components/pages/Contact"
